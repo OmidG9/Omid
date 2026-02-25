f62c84887ca09a658acb8354cb1b356fe21970d7
Omid Ghanbari <130829592+OmidG9@users.noreply.github.com>
2026-02-25 12:17:44 +0330

    feat(config): add allowedDevOrigins to nextConfig for development environment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['framer-motion'],
+  allowedDevOrigins: ['ghanbariomid.ir', 'www.ghanbariomid.ir'],
 };
 
 module.exports = nextConfig;
